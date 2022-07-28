03f138987f9eb2b4e809da45a94654d93752805c
ellaha23-meet <109908161+ellaha23-meet@users.noreply.github.com>
2022-07-28 16:05:10 +0300

    finished
    Thank you!

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,10 +4,37 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+@app.route('/', methods=['GET', 'POST'])
 def home():
-	
-	return render_template('home.html')
+	if request.method == 'POST':
+
+
+		login_session['quote'] = request.form['quote']
+		login_session['author'] = request.form['author']
+		login_session['age'] = request.form['age']
+
+		# quote = {'quote': 'your quote:'}
+		# x = requests.post(userquote = quote)
+		# print(x.text)
+		# name = {'name': 'the author:'}
+		# x = requests.post(username= name)
+		# print(x.text)
+		# age = {'age': 'hisage:'}
+		# x = requests.post(userage = age)
+		# print(x.text)
+		try:
+
+		    quote = login_session['quote']
+		    author= login_session['author']
+		    age = login_session['age']
+		    return redirect(url_for('thanks'))
+		except:
+		    print('try again')
+		    return redirect(url_for('home'))
+	else: 
+		return render_template('home.html')
+
+
 
 
 @app.route('/error')
@@ -18,13 +45,12 @@ def error():
 
 @app.route('/display')
 def display():
+    return render_template('display.html', quote = login_session['quote'], author = login_session['author'], age = login_session['age']) 
 
-	return render_template('display.html', ) # What variables are needed?
 
 
 @app.route('/thanks')
 def thanks():
-
 	return render_template('thanks.html')
 
 
